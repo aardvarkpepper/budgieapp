fdd54966fe04a9cfa23eaeb687995d541f0cf9d8
aardvarkpepper <aardvarkpepper@gmail.com>
2023-04-18 19:36:23 -0400

    Add transactions.js data to models folder.

---
+++ models/transactions.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id:  00001,
+        item_name: "Hamster Food",
+        amount: 1111,
+        date:  "2023-04-01",
+        from: "PetsRUs ",
+        category: "pets",
+    },
+    {
+        id:  00011,
+        item_name: "Chicken",
+        amount: 2222,
+        date:  "2023-04-02",
+        from: "Wal-Mart",
+        category: "food",
+    },
+    {
+        id:  00111,
+        item_name: "Programming for Dummies",
+        amount: 3333,
+        date:  "2023-04-03",
+        from: "Barnes & Noble",
+        category: "pets",
+    },
+]
